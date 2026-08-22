7ee55e50a3a3f8427bb2fa3906463f7c035ebab0
Namayjain <namayjain2@gmail.com>
2026-08-22 20:31:37 +0530

    feat: set default initial theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           <CustomCursor />
           <Loader />
           <Navbar />
